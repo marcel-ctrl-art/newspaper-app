dead41a30d714660f02718c6591578540b2ee336
Marcel <marceli.lagoda@gmail.com>
2020-02-01 02:35:35 +0100

    Add UserAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+
+admin.site.register
