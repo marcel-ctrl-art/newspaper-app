0b58784ee8a51843aced3c56344e28437d17aa37
Marcel <marceli.lagoda@gmail.com>
2020-02-02 04:44:14 +0100

    Include pages.urls in the project level urls

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include("pages.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
