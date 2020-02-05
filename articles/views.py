eff23be849cfa46463ff4deb3882c38be8459a04
Marcel <marceli.lagoda@gmail.com>
2020-02-05 15:46:47 +0100

    Add List View of all articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .modles import Article
+
+class ArticlesListView(ListView):
+    model = Article
+    paginate_by = 3
+    template_name = 'articles_list.html'
