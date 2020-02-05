515c7496a60a90e0561e5e5bafdadc717506ba4c
Marcel <marceli.lagoda@gmail.com>
2020-02-05 04:53:40 +0100

    Include articles.urls within main project urls

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include("pages.urls")),
-    path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
 ]
