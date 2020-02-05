c5455f7827f024ea26d98b931b910858e4e6ab93
Marcel <marceli.lagoda@gmail.com>
2020-02-06 00:30:43 +0100

    Import & add LoginRequiredMixin to all exisitng views

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, DeleteView, UpdateView, ListView
@@ -5,30 +6,35 @@ from django.views.generic import CreateView, DetailView, DeleteView, UpdateView,
 from .models import Article
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = 'article_create.html'
     model = Article
-    fields = ['title', 'text', 'author']
+    fields = ['title', 'text']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'article_delete.html'
     model = Article
-    success_url = reverse_lazy('article_list')
+    success_url = reverse_lazy('articles_list')
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = 'article_detail.html'
     model = Article
 
-class ArticlesListView(ListView):
+
+class ArticlesListView(LoginRequiredMixin, ListView):
     model = Article
     paginate_by = 3
     template_name = 'articles_list.html'
     context_object_name = 'articles'
 
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin, UpdateView):
     model = Article
-    fields = ['title', 'text']
     template_name = 'article_edit.html'
+    fields = ['title', 'text']
