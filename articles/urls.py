d1a3446dc3988fa9df31b8b35ba02ed40ab3933d
Marcel <marceli.lagoda@gmail.com>
2020-02-05 19:04:30 +0100

    Update articles.urls with Create New Article

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.ArticlesListView.as_view(), name='articles_list'),
     path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
-    path('<int:pk>/', views.ArticleEditView.as_view(), name='article_edit'),
+    path('<int:pk>/edit/', views.ArticleEditView.as_view(), name='article_edit'),
+    path('new/', views.ArticleCreateView.as_view(), name='article_create'),
 ]
