7451de3645d18546271857d2f5cd82966c3b61df
Marcel <marceli.lagoda@gmail.com>
2020-02-05 18:16:31 +0100

    Add urls for (article) delete, detail & edit views

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticlesListView.as_view(), name='articles_list'),
+    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/', views.ArticleEditView.as_view(), name='article_edit'),
 ]
