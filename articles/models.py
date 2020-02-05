254eb22638568d6d62615e736c408f7b89b6de6e
Marcel <marceli.lagoda@gmail.com>
2020-02-05 23:16:15 +0100

    Rename templates for article editing

--- articles/models.py
+++ articles/models.py
@@ -13,4 +13,4 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('article_detail', arg=[str(self.id)])
+        return reverse('article_detail', args=[str(self.id)])
