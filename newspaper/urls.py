a80f7a0927b9eedf38bac24405b48aa46d262465
Marcel <marceli.lagoda@gmail.com>
2020-02-23 04:06:36 +0100

    Add PasswordResetView url from django.contrib.auth

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
